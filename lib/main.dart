dfdaab995d2ebf95656017d21819d161b7e45b4e
Erimsah Olut <erimsaholut@icloud.com>
2023-11-06 11:18:11 +0300

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-
 import 'login_page.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,4 +25,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
